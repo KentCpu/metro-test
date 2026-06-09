664ce43d9f913183ee4ca3f3c9c679cb5c3718a5
KentCpu <ruslanmirzaev16621@gmail.com>
2026-06-09 13:07:07 +0700

    feat: modal component

---
+++ src/shared/ui/Modal.tsx
@@ -0,0 +1,5 @@
+import { Modal as UIModal, type ModalProps } from "@mantine/core";
+
+export function Modal(props: ModalProps) {
+  return <UIModal {...props} />;
+}
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,4 @@
-export { MapGL } from './MapGL';
-export { Button } from './Button';
-export { TextInput } from './TextInput';
+export { MapGL } from "./MapGL";
+export { Button } from "./Button";
+export { TextInput } from "./TextInput";
+export { Modal } from "./Modal";
