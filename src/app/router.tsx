2862c5a98639198887eca2011c6694c97cf7e6f4
KentCpu <ruslanmirzaev16621@gmail.com>
2026-06-09 10:49:17 +0700

    chore: initial project setup

---
+++ src/app/router.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import { HomePage } from "../pages/home";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />,
+	},
+]);
+
+export function Router() {
+	return <RouterProvider router={router} />;
+}
