f0205dcb774ae55860a92ad43e8322d44a7715cd
KentCpu <ruslanmirzaev16621@gmail.com>
2026-06-09 13:04:33 +0700

    feat: text input component

---
+++ src/shared/ui/TextInput.tsx
@@ -0,0 +1,6 @@
+import { TextInput as UITextInput } from "@mantine/core";
+import type { TextInputProps } from "@mantine/core";
+
+export function TextInput(props: TextInputProps) {
+  return <UITextInput {...props} />;
+}
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,2 +1,3 @@
 export { MapGL } from './MapGL';
 export { Button } from './Button';
+export { TextInput } from './TextInput';
